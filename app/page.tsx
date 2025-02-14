4eb3d7a10231b19a587c137bbd02d0864af3d64b
mahutt <tommy.mahut@gmail.com>
2025-02-14 00:18:24 -0500

    Add projects tab

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        mahutt's Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`My name is Thomas and I love to code. I've been watching Severance
+        lately and man that show is good. Yes, this is a Vercel portfolio/blog
+        template. No, I'm not ashamed.`}
       </p>
       <div className="my-8">
         <BlogPosts />
---
+++ app/projects/page.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Projects',
+  description: 'This is a list of my projects.',
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        My Projects
+      </h1>
+      <p className="mb-4">{`I'm working on it.`}</p>
+    </section>
+  )
+}
