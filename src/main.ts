967f255d80bbe209286c16d0f12b350ed866e80e
mahutt <tommy.mahut@gmail.com>
2025-10-10 20:06:55 -0400

    Remove button counter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,1 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Thomas Mahut</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      This portfolio is under construction.
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
