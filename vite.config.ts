8e8b41b0d39a3760ecce1a2cc09c4eded44bc5f4
mahutt <tommy.mahut@gmail.com>
2025-10-12 17:41:43 -0400

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: '/index.html',
-        work: '/work.html',
-      },
-    },
-  },
 })
