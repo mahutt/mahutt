82bc552cc6a8593c9c4689c69d0749366031d8c6
mahutt <tommy.mahut@gmail.com>
2025-10-20 02:40:36 -0400

    Clean up /blogs index

--- src/scripts/links.ts
+++ src/scripts/links.ts
@@ -14,15 +14,15 @@ export default function setupLinks() {
       a.href = link.url
       a.target = '_blank'
       a.rel = 'noopener'
-      a.classList.add('transition', 'duration-200', 'hover:scale-110')
+      a.classList.add('transition', 'duration-200', 'hover:scale-110', 'social')
       a.onmouseenter = () => {
-        document.querySelectorAll('a').forEach((el) => {
+        document.querySelectorAll('.social').forEach((el) => {
           el.classList.add('opacity-50')
         })
         a.classList.remove('opacity-50')
       }
       a.onmouseleave = () => {
-        document.querySelectorAll('a').forEach((el) => {
+        document.querySelectorAll('.social').forEach((el) => {
           el.classList.remove('opacity-50')
         })
       }
